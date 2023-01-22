97a145f6d416cc89d250d64ea6f4b02c5ac6c849
CristinaColomoiets <colomoetscristina@gmail.com>
2023-01-22 22:09:30 +0100

    refactor terminado para que funcione todo correctamente

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 'use strict'
 
+/**
+ * Para parar la columna:
+ * Cuando nuestro wrapper se salga de la pantalla.
+ *      Creamos un elemento en la parte de abajo de nuestra columna
+ *      Le ponemos un intersection observer
+ *      Cuando deje de intersectar sabremos que ha llegado al final.
+ * Cuando llegue a ese limite se empiezan a borrar elementos empezando desde el ultimo generado
+ *      Guardamos su altura del elemento que vamos a sacar
+ *      Borramos el elemento
+ *      Empujamos el wrapper (columna) hacia abajo con la altura guardada
+ * 
+ */
+
 const elementsNumber = 100;
-const velocity = 10;
+const velocity = 50;
 
 function createMatrixNodeHTML(layerSelector){
-    let top = Math.trunc(Math.random () * 100);
+    let top = Math.trunc(Math.random () * 100); // 100 es %
     let left = Math.trunc(Math.random () * 100);
-    let layer = document.querySelector(layerSelector);
+    let layerHTML = document.querySelector(layerSelector);
 
     let html_wrapper_numbers = document.createElement('div');
     html_wrapper_numbers.style.top = top * -1 + '%';
@@ -15,7 +28,7 @@ function createMatrixNodeHTML(layerSelector){
 
     add_numbers(html_wrapper_numbers);
 
-    layer.appendChild(html_wrapper_numbers);
+    layerHTML.appendChild(html_wrapper_numbers);
 
 
     return html_wrapper_numbers;
@@ -35,9 +48,31 @@ function create_box_number(){
 }
 
 function add_numbers(node_wrapper){
+    let passedWindowMargin = false;
+    const initialTop = node_wrapper.style.top;
+    let pixelsAdded = 0;
     let interval = setInterval(function(){
-        let nodeNumber = create_box_number();
-        node_wrapper.appendChild(nodeNumber);
+        if (passedWindowMargin || window.screen.availHeight < node_wrapper.offsetHeight + node_wrapper.offsetTop) {
+            const childNodesLength = node_wrapper.childNodes.length;
+            // node_wrapper.removeChild(node_wrapper.childNodes[childNodesLength -1]);
+            const elementSizeBeforeRemove = node_wrapper.offsetHeight;
+
+            node_wrapper.childNodes[childNodesLength -1].remove(); // Es lo mismo que en la linea 54
+            const elementSizeAfterRemove = node_wrapper.offsetHeight;
+            let sizeDiferenceAfterRemove = elementSizeBeforeRemove - elementSizeAfterRemove;
+            const pixelsToAdd = pixelsAdded + sizeDiferenceAfterRemove;
+            node_wrapper.style.top = `calc(${initialTop} + ${pixelsAdded}px`;
+            pixelsAdded = pixelsToAdd;
+            passedWindowMargin = true;
+            if (childNodesLength < 2) {
+                node_wrapper.style.top = initialTop;
+                passedWindowMargin = false;
+                pixelsAdded = 0;
+            }            
+        }else{
+            let nodeNumber = create_box_number();
+            node_wrapper.appendChild(nodeNumber);
+        }
     }, velocity);
 }
 
